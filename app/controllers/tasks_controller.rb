8806dcdbd175a3b20183f0b35baf97789fe911c8
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2016-07-13 13:00:15 +0200

    add new and create to CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,13 +6,24 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
   def new
+    @task = Task.new
   end
   def create
+    task = Task.new(task_params)
+    task.completed = false
+    task.save
+    redirect_to tasks_path
   end
   def edit
   end
   def update
   end
   def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   get 'tasks' => 'tasks#index'
-  get 'tasks/:id' => 'tasks#show', as: :task
   get 'tasks/new' => 'tasks#new'
+  get 'tasks/:id' => 'tasks#show', as: :task
   post 'tasks' => 'tasks#create'
   get 'tasks/:id/edit' => 'tasks#edit'
   patch 'tasks/:id' => 'tasks#update'
